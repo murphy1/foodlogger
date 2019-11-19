202441bc020f15f07187748faa6b0f3484708c68
murphy1 <smurphy54321@gmail.com>
2019-11-19 13:39:21 -0500

    Initial commit

---
+++ src/main/java/com/murphy1/foodlogger/FoodloggerApplication.java
@@ -0,0 +1,13 @@
+package com.murphy1.foodlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodloggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodloggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/murphy1/foodlogger/FoodloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murphy1.foodlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
