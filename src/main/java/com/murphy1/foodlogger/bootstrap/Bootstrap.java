97a4fd6d52998d5a203e54e21c0ce16a10765207
murphy1 <smurphy54321@gmail.com>
2019-11-25 18:22:24 -0500

    User Object now created. Closes #2

--- src/main/java/com/murphy1/foodlogger/bootstrap/Bootstrap.java
+++ src/main/java/com/murphy1/foodlogger/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package com.murphy1.foodlogger.bootstrap;
 
 import com.murphy1.foodlogger.model.Category;
 import com.murphy1.foodlogger.model.Product;
+import com.murphy1.foodlogger.model.User;
 import com.murphy1.foodlogger.repositories.CategoryRepository;
 import com.murphy1.foodlogger.repositories.ProductRepository;
+import com.murphy1.foodlogger.repositories.UserRepository;
 import lombok.Builder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,19 +16,24 @@ public class Bootstrap implements CommandLineRunner {
 
     private ProductRepository productRepository;
     private CategoryRepository categoryRepository;
+    private UserRepository userRepository;
 
-    public Bootstrap(ProductRepository productRepository, CategoryRepository categoryRepository) {
+    public Bootstrap(ProductRepository productRepository, CategoryRepository categoryRepository, UserRepository userRepository) {
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         productRepository.deleteAll().block();
         categoryRepository.deleteAll().block();
+        userRepository.deleteAll().block();
 
         productRepository.save(Product.builder().name("products").calories(250.0).description("A Great Product").build()).block();
 
         categoryRepository.save(Category.builder().name("Category1").description("Spicy").build()).block();
+
+        userRepository.save(User.builder().firstName("Stevie").lastName("Murphy").username("smurphy94").email("smurphy54321@gmail.com").password("password").build()).block();
     }
 }
---
+++ src/main/java/com/murphy1/foodlogger/model/User.java
@@ -0,0 +1,76 @@
+package com.murphy1.foodlogger.model;
+
+import lombok.Builder;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Builder
+public class User {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String id, String firstName, String lastName, String username, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
